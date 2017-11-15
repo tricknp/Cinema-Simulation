ffc987ecf8fb8d57dc7b59b2b686ef91c6dc830e
Patrick Porto <np_patrick@yahoo.com>
2017-11-15 01:16:10 -0200

    database

---
+++ cinema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS cinema;
+CREATE DATABASE IF NOT EXISTS cinema;
+
+USE cinema;
+
+CREATE TABLE IF NOT EXISTS sessao (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  sala INT(11) NOT NULL,
+  filme INT(11) NOT NULL,
+  data DATE NOT NULL,
+  horaInicio TIME NOT NULL,
+  capacidade INT(11) NOT NULL,
+  PRIMARY KEY (id)
+  );
