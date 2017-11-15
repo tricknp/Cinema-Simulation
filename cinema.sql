221d01e7f4eb23497ee0c5833b7f22dce344b7b8
Patrick Porto <np_patrick@yahoo.com>
2017-11-15 01:49:18 -0200

    database

--- cinema.sql
+++ cinema.sql
@@ -3,20 +3,13 @@ CREATE DATABASE IF NOT EXISTS cinema;
 
 USE cinema;
 
+DROP TABLE IF EXISTS sessao;
 CREATE TABLE IF NOT EXISTS sessao (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-<<<<<<< HEAD
-  sala INT(11) NOT NULL,
-  filme INT(11) NOT NULL,
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  sala VARCHAR(45) NOT NULL,
+  filme VARCHAR(45) NOT NULL,
+  capacidade INT(10) NOT NULL,
   data DATE NOT NULL,
   horaInicio TIME NOT NULL,
-  capacidade INT(11) NOT NULL,
-=======
-  sala INT(11),
-  filme INT(11),
-  data DATE,
-  horaInicio TIME,
-  capacidade INT(11),
->>>>>>> 363a6df11f76038903f09bd6277e88160dd266a5
   PRIMARY KEY (id)
   );
